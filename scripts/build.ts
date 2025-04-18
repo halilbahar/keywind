1b0e27f326bedfc0cd7e02423d4abf579c31f059
Halil Bahar <halil.s.bahar@gmail.com>
2025-04-18 17:03:20 +0200

    Make the keycloak theme name dynamic

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,7 +1,8 @@
 import archiver from 'archiver';
 import { createWriteStream, existsSync, mkdirSync } from 'fs';
+import themeMeta from '../META-INF/keycloak-themes.json';
 
-import { name } from '../package.json';
+const name = themeMeta.themes[0].name;
 
 const dir = 'out';
 const file = `${name}.jar`;
@@ -10,7 +11,6 @@ const path = `${dir}/${file}`;
 !existsSync(dir) && mkdirSync(dir);
 
 const output = createWriteStream(`${__dirname}/../${path}`);
-
 const archive = archiver('zip');
 
 archive.on('error', (error) => {
@@ -19,7 +19,10 @@ archive.on('error', (error) => {
 
 archive.pipe(output);
 
+// Copy META-INF as is
 archive.directory('META-INF', 'META-INF');
-archive.directory('theme', 'theme');
+
+// Copy theme folder but rename 'keywind' dynamically
+archive.directory(`theme/keywind`, `theme/${name}`);
 
 archive.finalize();
